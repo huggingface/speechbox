21675b6e07a00306c873ac48ee64031df8713504
Patrick von Platen <patrick.v.platen@gmail.com>
2022-12-26 18:24:35 +0100

    add license etc...

--- examples/restore.py
+++ examples/restore.py
@@ -4,9 +4,7 @@ from speechbox import Restorer
 
 restorer = Restorer.from_pretrained("openai/whisper-tiny.en")
 
-dataset = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean")[
-    "validation"
-]
+dataset = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean")["validation"]
 audio = dataset[0]["audio"]["array"]
 text = dataset[0]["text"].lower()
 
